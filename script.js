851ec85ceb0a79cc916c2cc4996d77f89e1fe423
khai96_ <hvksmr1996@gmail.com>
2017-03-10 14:01:25 +0700

    Clear all scripts

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
 'use strict'
 
-const aInput = document.getElementById('a')
-const bInput = document.getElementById('b')
-const kqCongOutput = document.getElementById('kq-cong')
-const kqTruOutput = document.getElementById('kq-tru')
-const kqNhanOutput = document.getElementById('kq-nhan')
-const kqChiaOutput = document.getElementById('kq-chia')
-
-const ketqua = () => {
-  const a = Number(aInput.value)
-  const b = Number(bInput.value)
-  kqCongOutput.textContent = a + b
-  kqTruOutput.textContent = a - b
-  kqNhanOutput.textContent = a * b
-  kqChiaOutput.textContent = a / b
-}
-
-document.getElementById('calc').addEventListener('click', ketqua, false)
-aInput.addEventListener('change', ketqua, false)
-bInput.addEventListener('change', ketqua, false)
+// Your code here...
