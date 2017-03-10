52bc4c3276998db339995bc676c3f44570c4f17f
Cuong <cuong8321@gmail.com>
2017-03-10 16:51:06 +0700

    Complete

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 const expression = document.getElementById('expression')
 const output = document.getElementById('output')
 
+expression.addEventListener('change', result, false)
 document.getElementById('calculate').addEventListener('click', result, false)
 
 function result () {
-  output.textContent = eval(expression.value)
+  try {
+    output.textContent = eval(expression.value)
+  } catch (error) {
+    output.textContent = error
+  }
 }
